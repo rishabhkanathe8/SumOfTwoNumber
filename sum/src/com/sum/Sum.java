8a5e8b0c467d7f9f4ba479631a3d761bf3dc9962
unknown <shaileshpasare5585@gmail.com>
2023-02-27 12:02:44 +0530

    firstcommit

---
+++ sum/src/com/sum/Sum.java
@@ -0,0 +1,20 @@
+package com.sum;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the nummber");
+
+		int num1 = sc.nextInt();
+		System.out.println("Enter the second Number");
+		int num2 = sc.nextInt();
+
+		int sum = num1 + num2;
+
+		System.out.println("The sum is " + sum);
+
+	}
+}
